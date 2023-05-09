070514981c0000f2d4081848197bcf73d8f091e9
Lương Xuân Thắng <luongthang730@gmail.com>
2023-05-09 17:19:37 +0700

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -10,6 +10,28 @@ class User {
         res.json(err);
       });
   }
+  
+  // [POST] /user/addUser
+    addUser(req, res, next) {
+        const { idUser, idToy, image, nameToy, price, quantity, totalPrice } = req.body;
+        UserModel.create({
+            idUser,
+            idToy,
+            image,
+            nameToy,
+            price,
+            quantity,
+            totalPrice,
+            state: true,
+        })
+            .then((user) => {
+                res.json(user);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    }
+  
   getUserById(req, res, next) {
     UserModel.find({ _id: req.params.id })
       .then((result) => {
