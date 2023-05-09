3fb2e756f314649fb2d933e219bc1fa10c6062e9
Lương Xuân Thắng <luongthang730@gmail.com>
2023-05-09 17:09:14 +0700

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -6,6 +6,7 @@ const AccountController = require("../controllers/Account");
 router.get("/getAccount/:id", AccountController.getAccount);
 router.post("/login", AccountController.login);
 router.post("/sign-up", AccountController.signup);
+router.post("/addAccount", AccountController.addAccount);
 router.put("/update/:id", AccountController.update);
 
 module.exports = router;
